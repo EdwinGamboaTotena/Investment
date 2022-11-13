aea23b7d841dd7c67ab1726c4c897d86b893a350
Edwin Gamboa <edwin199617@gmail.com>
2022-11-13 14:46:52 -0500

    Se agrega nombre personalizado a las tablas

--- src/main/java/com/credence/investment/infraestructure/payment/PaymentEntity.java
+++ src/main/java/com/credence/investment/infraestructure/payment/PaymentEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "payments")
 @Data
 @Builder
 @NoArgsConstructor
--- src/main/java/com/credence/investment/infraestructure/project/ProjectEntity.java
+++ src/main/java/com/credence/investment/infraestructure/project/ProjectEntity.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "projects")
 @Data
 @Builder
 @NoArgsConstructor
